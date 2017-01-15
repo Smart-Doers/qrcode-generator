06ccf613bae8c56b44b54c1d83fe42466d229864
grijesh <grijesh.rajoria@gmail.com>
2017-01-15 20:46:02 +0000

    Functional test cases fix

--- webservice/src/main/java/com/qrcode/web/services/generator/config/SwaggerConfig.java
+++ webservice/src/main/java/com/qrcode/web/services/generator/config/SwaggerConfig.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
+@Configuration
 @EnableAutoConfiguration
+@Import(SpringSwaggerConfig.class)
 public class SwaggerConfig {
 
     private SpringSwaggerConfig springSwaggerConfig;
